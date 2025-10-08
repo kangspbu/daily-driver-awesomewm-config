68b9b5f1822fd5065f72dc24b65275bc955597c6
kangspbu <seljaworks@gmail.com>
2025-10-08 20:35:25 +0700

    fixes and remove useless file

--- config/apps.lua
+++ config/apps.lua
@@ -2,7 +2,7 @@
 -- APPLICATION DEFINITIONS (GLOBAL VARIABLES)
 -- ============================================================================
 
-terminal = "st -f \"FiraCode Nerd Font:style=Regular:size=16\""
+terminal = "st"
 browser = "brave --ignore-gpu-blocklist --enable-zero-copy --enable-features=VaapiVideoDecodeLinuxGL,AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder,VaapiIgnoreDriverChecks"
 rofi = "rofi -show drun"
 --editor = os.getenv("EDITOR") or "xed"
