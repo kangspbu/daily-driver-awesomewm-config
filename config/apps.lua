318a482f9ac2fcd3499dd8680211a26cbd596c28
kangspbu <seljaworks@gmail.com>
2025-10-12 13:56:40 +0700

    change default terminal

--- config/apps.lua
+++ config/apps.lua
@@ -2,11 +2,10 @@
 -- APPLICATION DEFINITIONS (GLOBAL VARIABLES)
 -- ============================================================================
 
-terminal = "st"
+terminal = "alacritty"
 --browser = "brave --ignore-gpu-blocklist --enable-zero-copy --enable-features=VaapiVideoDecodeLinuxGL,AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder,VaapiIgnoreDriverChecks"
 browser_soos = "brave --profile-directory='Default' --ignore-gpu-blocklist --enable-zero-copy --enable-features=VaapiVideoDecodeLinuxGL,AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder,VaapiIgnoreDriverChecks"
 browser_work = "brave --profile-directory='Profile 1' --ignore-gpu-blocklist --enable-zero-copy --enable-features=VaapiVideoDecodeLinuxGL,AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder,VaapiIgnoreDriverChecks"
-
 rofi = "rofi -show drun"
 --editor = os.getenv("EDITOR") or "xed"
 editor = "xed"
